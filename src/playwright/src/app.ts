060d625d985f83e9711e107790b5ff723de91c19
Shinichi Haramizu <shinichi.haramizu@sitecore.com>
2023-10-09 20:08:31 +0900

    update

---
+++ src/playwright/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import { hello } from "./routes/hello";
+import { screenshot } from "./routes/screenshot";
+
+const app = express();
+const port = 3000;
+
+// APIエンドポイント1
+app.get("/api/hello", hello);
+
+// APIエンドポイント2
+app.get("/api/screenshot", screenshot);
+
+// ルートパス('/')にアクセスした場合のハンドラー
+app.get("/", (req, res) => {
+  res.send("Welcome to the Sitecore Search Utility");
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
---
+++ src/playwright/src/routes/screenshot.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { chromium } from "playwright";
+
+const PAGE_WAIT_TIME = parseInt(process.env.PAGE_WAIT_TIME || "2000");
+const PAGE_WIDTH = parseInt(process.env.PAGE_WIDTH || "1280");
+const PAGE_HEIGHT = parseInt(process.env.PAGE_HEIGHT || "800");
+
+export const screenshot = async (req: Request, res: Response) => {
+  const urlParam = req.query.url as string; // URLパラメータを取得
+
+  if (!urlParam) {
+    return res.status(400).json({ error: "URL parameter is required" });
+  }
+
+  // コンソールにメッセージを送信
+  console.log(`Processing screenshot for URL: ${urlParam}`);
+
+  // Playwrightを使用してスクリーンショットを取得
+  const browser = await chromium.launch({
+    args: [`--env=PLAYWRIGHT_BROWSERS_PATH=0 --no-sandbox`],
+  });
+  const page = await browser.newPage();
+  await page.setViewportSize({ width: PAGE_WIDTH, height: PAGE_HEIGHT });
+  await page.goto(urlParam);
+  await page.waitForTimeout(PAGE_WAIT_TIME);
+
+  const screenshot = await page.screenshot();
+  // スクリーンショットをHTTPレスポンスとして送信
+  res.setHeader("Content-Type", "image/png");
+  res.send(screenshot);
+
+  await browser.close();
+  console.log("Screenshot taken");
+};
