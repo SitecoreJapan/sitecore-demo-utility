b8777b128b60fe0a40fd4c6ffdfbc3a5928b6e69
Shinichi Haramizu <shinichi.haramizu@sitecore.com>
2023-10-11 16:25:34 +0900

    change port number

--- src/playwright/src/app.ts
+++ src/playwright/src/app.ts
@@ -3,7 +3,7 @@ import { hello } from "./routes/hello";
 import { screenshot } from "./routes/screenshot";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000
 
 // APIエンドポイント1
 app.get("/api/hello", hello);
